10a619428bb20a20a9262e4f1f3362ba9944e5db
chanzhilin <1181785848@qq.com>
2019-03-29 18:07:05 +0800

    配置activity分发

--- app/src/main/java/chen/vms/MyApplication.java
+++ app/src/main/java/chen/vms/MyApplication.java
@@ -5,7 +5,7 @@ import android.support.multidex.MultiDexApplication;
 import com.alibaba.android.arouter.launcher.ARouter;
 
 /**
- * Introduce :
+ * Introduce :  自定义Application,替换默认的
  * Created by CHEN_ on 2019/3/20.
  * version:1.0.0
  **/
@@ -18,6 +18,7 @@ public class MyApplication extends MultiDexApplication {
             ARouter.openLog();
             ARouter.openDebug();
         }
-        ARouter.init(this);
+        ARouter.init(this);//初始化
     }
+
 }
